e93a04c3fde2244d93932acd0499c14ead3b7d0a
Alex <alexbondar03@gmail.com>
2026-05-15 16:05:36 +0300

    add v3 and v4

---
+++ Test5/optIndexes_v3.sql
@@ -0,0 +1,56 @@
+CREATE INDEX PBI.IX_ACCSTATUS_JOIN_COVER ON PBI."AccountStatus" (
+    "IDNAccount" ASC,
+    "AccountStatus" ASC,
+    "DtAccountOpen" ASC,
+    "DtAccountChange" ASC
+) using stogroup SGPBI priqty 7200 secqty 7200 DEFER YES ~
+
+CREATE INDEX PBI.IX_ACCSTATUS_OWNER_COVER ON PBI."AccountStatus" (
+    "IDNAccount" ASC,
+    "AccountStatus" ASC,
+    "StatusOwner" ASC,
+    "DtAccountOpen" ASC,
+    "DtAccountChange" ASC
+) using stogroup SGPBI priqty 9000 secqty 9000 DEFER YES ~
+
+CREATE INDEX PBI.IX_ACCOUNT_NB_BA4 ON PBI."Account" (
+    "NrBank" ASC,
+    "BalAccount4" ASC,
+    "IDNAccount" ASC
+) using stogroup SGPBI priqty 6400 secqty 6400 DEFER YES ~
+
+CREATE INDEX PBI.IX_YTIGOS_BANK ON PBI."TableYTIGos" (
+    "NrBankMQ" ASC
+) using stogroup SGPBI priqty 60 secqty 60 DEFER YES ~
+
+CREATE INDEX PBI.IX_YSRGOS_JOIN ON PBI."TableYSRGos" (
+    "NrAccount" ASC,
+    "CdCurrency" ASC,
+    "NrEWallet" ASC,
+    "NrBank" ASC,
+    "DtBalance" ASC
+) using stogroup SGPBI priqty 24 secqty 24 DEFER YES ~
+
+CREATE INDEX PBI.IX_SPBICBY_FILTER ON PBI."SPBICBY" (
+    "BICStatus" ASC,
+    "CdActRecord" ASC,
+    "NrBank" ASC,
+    "CdBank" ASC
+) using stogroup SGPBI priqty 12 secqty 12 DEFER YES ~
+
+CREATE INDEX PBI.IX_SPACCINC_FILTER ON PBI."SPAccGosInclude" (
+    "PrYSR" ASC,
+    "count_NrAccount" ASC,
+    "NrAccountOrder" ASC
+) using stogroup SGPBI priqty 12 secqty 12 DEFER YES ~
+
+CREATE INDEX PBI.IX_SPACCEXC_FILTER ON PBI."SPAccGosExclude" (
+    "PrYSR" ASC,
+    "count_NrAccount" ASC,
+    "NrAccountOrder" ASC
+) using stogroup SGPBI priqty 12 secqty 12 DEFER YES ~
+
+CREATE INDEX PBI.IX_SPDATESCTL_FILTER ON PBI."SPDatesControl" (
+    "PrYSR_Month" ASC,
+    "LastWorkDayMonth" DESC
+) using stogroup SGPBI priqty 12 secqty 12 DEFER YES ~
